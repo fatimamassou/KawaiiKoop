d048398bc02f9fdb08a286f00786bcc26d7538b5
fatmamath <massoufatima23@gmail.com>
2025-04-12 23:05:47 +0100

    add under text hero

--- resources/views/client/components/molecules/hero/text-block.blade.php
+++ resources/views/client/components/molecules/hero/text-block.blade.php
@@ -4,5 +4,11 @@
 
 <div class="text-center text-block-hero py-5">
     <h1>Cute, Fun, and Kawaii — Every Day!</h1>
-    <p>Autem eveniet eum delectus pariatur. Et maiores sit sed placeat quas voluptatum qui. Suscipit veritatis est provident illum commodi. Voluptas quos culpa. Nobis ex nihil laudantium.</p>
+    <p>
+        At KawaiiKoop, we bring you a carefully curated collection of all things 
+        adorable and anime-inspired. From plushies to accessories, our products 
+        are designed to add a little extra happiness to your life. Whether you're 
+        shopping for yourself or finding the perfect gift, we offer a world of kawaii
+        treasures to brighten your day.
+    </p>
 </div>
