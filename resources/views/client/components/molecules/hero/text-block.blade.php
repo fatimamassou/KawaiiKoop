82163440fe53444f69ccfc6e542ff398e126605b
fatmamath <massoufatima23@gmail.com>
2025-04-12 23:03:51 +0100

    add text hero

--- resources/views/client/components/molecules/hero/text-block.blade.php
+++ resources/views/client/components/molecules/hero/text-block.blade.php
@@ -3,6 +3,6 @@
 @endpush
 
 <div class="text-center text-block-hero py-5">
-    <h1>Sustainable yet stylish lifestye</h1>
+    <h1>Cute, Fun, and Kawaii — Every Day!</h1>
     <p>Autem eveniet eum delectus pariatur. Et maiores sit sed placeat quas voluptatum qui. Suscipit veritatis est provident illum commodi. Voluptas quos culpa. Nobis ex nihil laudantium.</p>
 </div>
