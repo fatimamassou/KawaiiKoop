587c1d91a952a4eaf30e4d753ea53ea237498895
fatmamath <massoufatima23@gmail.com>
2025-04-12 23:06:21 +0100

    change under text hero

--- resources/views/client/components/molecules/hero/text-block.blade.php
+++ resources/views/client/components/molecules/hero/text-block.blade.php
@@ -5,10 +5,7 @@
 <div class="text-center text-block-hero py-5">
     <h1>Cute, Fun, and Kawaii — Every Day!</h1>
     <p>
-        At KawaiiKoop, we bring you a carefully curated collection of all things 
-        adorable and anime-inspired. From plushies to accessories, our products 
-        are designed to add a little extra happiness to your life. Whether you're 
-        shopping for yourself or finding the perfect gift, we offer a world of kawaii
-        treasures to brighten your day.
+        Discover cute, anime-inspired products that bring joy to your life. 
+        Shop now and add a touch of kawaii to your world!
     </p>
 </div>
