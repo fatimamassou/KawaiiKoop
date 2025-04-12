b50b8aea3913cfc33fb13b55ba4742e3b5617a28
fatmamath <massoufatima23@gmail.com>
2025-04-12 23:10:19 +0100

    change first text hero

--- resources/views/client/components/molecules/hero/text-block.blade.php
+++ resources/views/client/components/molecules/hero/text-block.blade.php
@@ -3,7 +3,7 @@
 @endpush
 
 <div class="text-center text-block-hero py-5">
-    <h1>Cute, Fun, and Kawaii — Every Day!</h1>
+    <h1>Where Kawaii Meets Your Everyday!</h1>
     <p>
         Discover cute, anime-inspired products that bring joy to your life. 
         Shop now and add a touch of kawaii to your world!
