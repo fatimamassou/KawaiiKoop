757600678fd8e1ab112e0dae15ff84727c57f284
fatmamath <massoufatima23@gmail.com>
2025-04-13 17:32:18 +0100

    lint fillable parametre

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,7 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['shop_id', 'name', 'path'];
+    protected $fillable = [
+        'shop_id',
+        'name',
+        'path'
+    ];
 
     public function shop(){
         return $this->belongsTo(Shop::class);
