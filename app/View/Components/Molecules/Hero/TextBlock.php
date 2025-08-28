6e266ef700c67c3e26686ddb88e3e1b0e8468ff5
fatmamath <massoufatima23@gmail.com>
2025-08-28 20:03:13 +0100

    init the code

--- app/View/Components/Molecules/Hero/TextBlock.php
+++ app/View/Components/Molecules/Hero/TextBlock.php
@@ -11,6 +11,7 @@ class TextBlock extends Component
      *
      * @return void
      */
+
     public function __construct()
     {
         //
@@ -21,6 +22,7 @@ class TextBlock extends Component
      *
      * @return \Illuminate\Contracts\View\View|\Closure|string
      */
+    
     public function render()
     {
         return view('client.components.molecules.hero.text-block');
