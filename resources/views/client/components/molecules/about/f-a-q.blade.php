21ddaf2882485dac0468c2bbfa4a4c9bf005f33e
fatmamath <massoufatima23@gmail.com>
2025-09-01 11:20:43 +0100

    add traslation to japanese

--- resources/views/client/components/molecules/about/f-a-q.blade.php
+++ resources/views/client/components/molecules/about/f-a-q.blade.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-4 col-md-4 col-12">
-        <h3 class="font-primary"><u>FAQ</u></h3>
+        <h3 class="font-primary"><u>FAQ /よくある質問</u></h3>
       </div>
       <div class="col-lg-8 col-md-8 col-12">
         <div class="accordion" id="accordionExample">
