0df0c49be388840e9762edf6c54ee61a683b2284
fatmamath <massoufatima23@gmail.com>
2025-09-01 11:21:21 +0100

    add traslation to japanese

--- resources/views/client/components/molecules/about/warranty.blade.php
+++ resources/views/client/components/molecules/about/warranty.blade.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-4 col-md-4 col-12">
-        <h3 class="font-primary"><u>Warranty</u></h3>
+        <h3 class="font-primary"><u>Warranty / 保証</u></h3>
       </div>
       <div class="col-lg-8 col-md-8 col-12">
         <p class="font-secondary">At Kawaiikoop, we pour love into every item we create! 💕 That’s why we offer a 30-day limited warranty on all products. If your item arrives damaged or has a manufacturing defect, we’ll gladly replace it or offer a store credit. Just reach out with your order number and a photo—we’re here to make things right!</p>
