f65b4fb5018086f065b0db3325515151b9e8d523
fatmamath <massoufatima23@gmail.com>
2025-09-01 11:15:30 +0100

    init the code

--- app/View/Components/Organisms/Products.php
+++ app/View/Components/Organisms/Products.php
@@ -24,6 +24,7 @@ class Products extends Component
      *
      * @return \Illuminate\Contracts\View\View|\Closure|string
      */
+    
     public function render()
     {
         return view('client.components.organisms.products');
