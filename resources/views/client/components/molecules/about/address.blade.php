392acd612e4320d7326eae2990a6e58c8e44e3ee
fatmamath <massoufatima23@gmail.com>
2025-09-01 11:20:22 +0100

    add traslation to japanese

--- resources/views/client/components/molecules/about/address.blade.php
+++ resources/views/client/components/molecules/about/address.blade.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-4 col-md-4 col-12">
-        <h3 class="font-primary"><u>Address</u></h3>
+        <h3 class="font-primary"><u>Address / 住所</u></h3>
       </div>
       <div class="col-lg-8 col-md-8 col-12">
         <div class="responsive-map">
