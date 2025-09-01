e179778ab2b1fa0f4fd9d9b92dbaf64a5c4603f2
fatmamath <massoufatima23@gmail.com>
2025-09-01 11:21:01 +0100

    add traslation to japanese

--- resources/views/client/components/molecules/about/shipping-returns.blade.php
+++ resources/views/client/components/molecules/about/shipping-returns.blade.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-4 col-md-4 col-12">
-        <h3 class="font-primary"><u>Shipping & Returns</u></h3>
+        <h3 class="font-primary"><u>Shipping & Returns / 配送と返品</u></h3>
       </div>
       <div class="col-lg-8 col-md-8 col-12">
         <p class="font-secondary">We ship worldwide from our kawaii HQ! ✈️ Orders are processed within 2–4 business days. Shipping times vary depending on your location, but we promise it’s worth the wait!
