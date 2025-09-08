24a57293ed5a18fb42ee51a9181bc8916a26dcd2
fatmamath <massoufatima23@gmail.com>
2025-09-08 15:10:25 +0100

    change the struction

--- resources/views/client/components/molecules/navbar/menu.blade.php
+++ resources/views/client/components/molecules/navbar/menu.blade.php
@@ -3,15 +3,15 @@
     <a href="/" class="nav-link">Home / 家</a>
   </li>
   <li class="nav-item">
-    <a href="{{ route('clientProducts') }}" class="nav-link">Products / 製品</a>
+    <a href="{{ route('clientProducts') }}" class="nav-link">Products/製品</a>
   </li>
   <li class="nav-item">
-    <a href="{{ route('clientCategory') }}" class="nav-link">Category / カテゴリ</a>
+    <a href="{{ route('clientCategory') }}" class="nav-link">Category/カテゴリ</a>
   </li>
   <li class="nav-item">
-    <a href="{{ route('clientAbout') }}" class="nav-link">About / について</a>
+    <a href="{{ route('clientAbout') }}" class="nav-link">About/について</a>
   </li>
   <li class="nav-item">
-    <a href="{{ route('clientCheckOrder') }}" class="nav-link">Check Order / 注文の確認</a>
+    <a href="{{ route('clientCheckOrder') }}" class="nav-link">Check Order/注文の確認</a>
   </li>
 </ul>
